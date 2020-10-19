7a5729fec1df0b1f25dcf05078ac112cb8266ebf
Thomas (Noit) <40148361+Noitcereon@users.noreply.github.com>
2020-10-19 11:19:41 +0200

    Created the MusicRecord model.

--- DRMusicLib/MusicRecord.cs
+++ DRMusicLib/MusicRecord.cs
@@ -6,6 +6,35 @@ namespace DRMusicLib
 {
     public class MusicRecord
     {
+        private string _title;
+        private string _artist;
+        private DateTimeOffset _yearOfPublication;
+        private int _durationInSeconds;
+        private bool _isCertifiedPlatinum;
 
+        public MusicRecord()
+        {
+
+        }
+        public MusicRecord(string title, string artist, DateTimeOffset yearOfPublication, int durationInSeconds, bool isCertifiedPlatinum)
+        {
+            _title = title;
+            _artist = artist;
+            _yearOfPublication = yearOfPublication;
+            _durationInSeconds = durationInSeconds;
+            _isCertifiedPlatinum = isCertifiedPlatinum;
+        }
+
+        public string Title => _title;
+        public string Artist => _artist;
+        public DateTimeOffset YearOfPublication => _yearOfPublication;
+        public int DurationInSeconds => _durationInSeconds;
+        public bool IsCertifiedPlatinum => _isCertifiedPlatinum;
+
+        public override string ToString()
+        {
+            return $"{Title} - {Artist}";
+        }
     }
+
 }
