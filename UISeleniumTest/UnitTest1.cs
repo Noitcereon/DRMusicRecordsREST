aab4edf289762d72cdf7c0d989653bc1f1e02d58
Thomas (Noit) <40148361+Noitcereon@users.noreply.github.com>
2020-10-22 13:51:22 +0200

    Simple GetAll test completed

--- UISeleniumTest/UnitTest1.cs
+++ UISeleniumTest/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
 namespace UISeleniumTest
@@ -9,12 +10,15 @@ namespace UISeleniumTest
     public class UnitTest1
     {
         private static readonly string Directory = @"C:\Users\mtlau\source\repos\DRMusicRecordsREST\UISeleniumTest\Drivers\";
+        //private static readonly string Directory = @"C:\Users\tba\source\repos\3_semester\DRMusicRecordsREST\UISeleniumTest\Drivers";
+
 
         private static IWebDriver _driver;
         [ClassInitialize]
         public static void Initialize(TestContext context)
         {
             _driver = new ChromeDriver(Directory);
+            //_driver = new FirefoxDriver(Directory);
         }
 
         [ClassCleanup]
@@ -23,13 +27,13 @@ namespace UISeleniumTest
             _driver.Dispose();
         }
         [TestMethod]
-        public void TestMethod1()
+        public void GetAllTest()
         {
-            _driver.Navigate().GoToUrl("http://localhost:3002/");
+            _driver.Navigate().GoToUrl("http://localhost:3000/");
             Assert.AreEqual("DR Music Records", _driver.Title);
 
             IWebElement GetAllBtn = _driver.FindElement(By.Id("GetAllRecordsBtn"));
-            IWebElement Content = _driver.FindElement(By.Id("ListUL"));
+            IWebElement Content = _driver.FindElement(By.Id("GetAllUL"));
             
             Assert.AreEqual("",Content.Text);
 
